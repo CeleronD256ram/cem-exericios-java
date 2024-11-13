eaf69d70c75355009e8f9bcb7560617424599a26
Tiago Vieira <celerond256@outlook.com>
2024-11-13 20:17:15 -0300

    exercicio02

--- src/main/java/com/cemexercicios/strings/exe01.java
+++ src/main/java/com/cemexercicios/strings/exe01.java
@@ -1,4 +1,17 @@
 package com.cemexercicios.strings;
 
+import java.util.Scanner;
+
 public class exe01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String palavra1 = sc.nextLine();
+        String palavra2 = sc.nextLine();
+
+        System.out.println("Palavra resultante: " + palavra1 + " " + palavra2);
+
+    }
+
+
 }
