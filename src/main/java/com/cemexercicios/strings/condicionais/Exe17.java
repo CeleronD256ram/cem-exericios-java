67d3908c1f91f608f9fb52a0d72a317408b1a22a
Tiago Vieira <celerond256@outlook.com>
2024-11-13 22:57:25 -0300

    exercicio 18

--- src/main/java/com/cemexercicios/strings/condicionais/Exe17.java
+++ src/main/java/com/cemexercicios/strings/condicionais/Exe17.java
@@ -1,4 +1,24 @@
 package com.cemexercicios.strings.condicionais;
 
+import java.util.Scanner;
+
 public class Exe17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //verifica se o número é positivo
+
+        System.out.println("Digite um número: ");
+        int numero = sc.nextInt();
+
+        if (numero < 0) {
+            System.out.println(numero + " é negativo");
+
+        } else if (numero > 0) {
+            System.out.println(numero + " é positivo");
+        } else {
+            System.out.println("Número digitado foi 0");
+        }
+    }
+
+
 }
--- src/main/java/com/cemexercicios/strings/condicionais/Exe18.java
+++ src/main/java/com/cemexercicios/strings/condicionais/Exe18.java
@@ -1,4 +1,27 @@
 package com.cemexercicios.strings.condicionais;
 
+import java.util.Scanner;
+
 public class Exe18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Receber duas notas, tirar a media e verificar se o aluno foi aprovado(média mairo que 6)
+
+        System.out.println("Nota 1: ");
+        double nota1 = sc.nextDouble();
+        System.out.println("Nota 2: ");
+        double nota2 = sc.nextDouble();
+
+        double media = (nota1 + nota2) / 2;
+        if (media > 6) {
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+
+
+
+
+
+    }
 }
